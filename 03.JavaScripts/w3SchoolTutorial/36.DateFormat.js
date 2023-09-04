1c54603be8e5dcb78eaa75cb277a9ab41f556de4
saeid <saeidebrahimiapply@gmail.com>
2023-09-04 13:52:07 +0330

    Making SASS Project Responsive

--- 03.JavaScripts/w3SchoolTutorial/36.DateFormat.js
+++ 03.JavaScripts/w3SchoolTutorial/36.DateFormat.js
@@ -6,19 +6,22 @@ const d11 = new Date("2019-07")
 document.getElementById("demo").innerHTML += d11 + "<br>"
 const d111 = new Date("2019")
 document.getElementById("demo").innerHTML += d111 + "<br>"
+
 // Short Format
 // Short dates are written with an "MM/DD/YYYY"
 const d2 = new Date("04/20/2015")
 document.getElementById("demo").innerHTML += d2 + "<br>"
+
 // Long Format
 // Long dates are most often written with a "MMM DD YYYY" syntax
 // Month and day can be in any order
 // month can be written in full (January), or abbreviated (Jan)
-// Commas are ignored. Names are case insensitive
+// Commas are ignored. Names are case-insensitive
 const d3 = new Date("Mar 25 2015" )
 const d4 = new Date("25 March 2015")
 document.getElementById("demo").innerHTML += d3 + "<br>"
 document.getElementById("demo").innerHTML += d4 + "<br>"
+
 // ISO dates can be written with added hours,
 // minutes, and seconds (YYYY-MM-DDTHH:MM:SSZ):
 // If you want to modify the time relative to UTC,
@@ -29,6 +32,7 @@ const d5 = new Date("2018-08-19T12:24:18Z")
 const d6 = new Date("2018-08-19T12:24:18+03:30")
 document.getElementById("demo").innerHTML += d5 + "<br>"
 document.getElementById("demo").innerHTML += d6 + "<br>"
+
 // UTC (Universal Time Coordinated)
 // is the same as GMT (Greenwich Mean Time).
 // If a date/time is created in GMT (Greenwich Mean Time), the date/time
