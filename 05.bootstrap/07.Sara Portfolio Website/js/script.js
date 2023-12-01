4a3f043d3dd6ae5c3899587e5baa82be07ae7b99
saeid <saeidebrahimiapply@gmail.com>
2023-12-01 23:20:36 +0330

    add new boostrap websites

--- 05.bootstrap/07.Sara Portfolio Website/js/script.js
+++ 05.bootstrap/07.Sara Portfolio Website/js/script.js
@@ -1,1 +1,15 @@
-// Add custom JavaScript here
+function runTypingEffect(){
+    const text = "I am Sara Bennet."
+    const typingElement = document.getElementById('typing-text')
+    const typingDelay = 100;
+    typeText(text,  typingElement, typingDelay)
+}
+function typeText(text, typingElement, delay){
+    for (let i=0; i< text.length; i++){
+        setTimeout(()=> {
+            typingElement.textContent += text[i]
+        }, delay*i)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect)
