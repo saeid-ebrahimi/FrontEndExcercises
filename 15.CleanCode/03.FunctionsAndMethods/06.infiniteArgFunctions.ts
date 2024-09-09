b83baae01b96a76f93bf56cac5eeb3bd315539f7
saeid <saeidebrahimiapply@gmail.com>
2024-09-09 22:56:33 +0330

    chnage splitting code in clean code Functions and methods folder

--- 15.CleanCode/03.FunctionsAndMethods/06.infiniteArgFunctions.ts
+++ 15.CleanCode/03.FunctionsAndMethods/06.infiniteArgFunctions.ts
@@ -1,23 +1,21 @@
 function sumUp(...numbers: number[]) {
-    let sum = 0;
-    for (const number of numbers)
-    {
-        sum += number;
-    }
-    return sum;
+  let sum = 0;
+  for (const number of numbers) {
+    sum += number;
+  }
+  return sum;
 }
 
 function sumUp2(numbers: number[]) {
-    let sum = 0;
-    for (const number of numbers)
-    {
-        sum += number;
-    }
-    return sum;
+  let sum = 0;
+  for (const number of numbers) {
+    sum += number;
+  }
+  return sum;
 }
 
-const total = sumUp(10, 10, 20, 23, 12)
-console.log(total)
+const total = sumUp(10, 10, 20, 23, 12);
+console.log(total);
 
-const total2 = sumUp2([10, 10, 20, 23, 12])
-console.log(total2)
+const total2 = sumUp2([10, 10, 20, 23, 12]);
+console.log(total2);
