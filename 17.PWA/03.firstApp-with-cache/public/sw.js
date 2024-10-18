ede6f8c30abeb5e43a0ffe4cab625275632192e0
saeid <saeidebrahimiapply@gmail.com>
2024-10-19 00:09:57 +0330

    make caching more optimistic

--- 17.PWA/03.firstApp-with-cache/public/sw.js
+++ 17.PWA/03.firstApp-with-cache/public/sw.js
@@ -1,12 +1,13 @@
+var STATIC_CACHE_NAME = 'static-v4'
+var DYNAMIC_CACHE_NAME = "dynamic-v2"
 // service worker scope is depends on the folder which exists
 self.addEventListener('install', function (event) {
     console.log('%c [Service Worker] Installing Service Worker ...', "background:green; color:white;padding:3px;", event);
     event.waitUntil(
-        caches.open("staticCache")
+        caches.open(STATIC_CACHE_NAME)
             .then(function (cache) {
                 console.log("[Service Worker] Pre Caching App Shell");
                 // cache.add("/")
-
                 cache.addAll([
                     "/",
                     "/index.html",
@@ -22,7 +23,6 @@ self.addEventListener('install', function (event) {
                     "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
                     // adding polyfills are unnecessary
                     // because old browser don't support service workers "src/js/promise.js", "src/js/fetch.js"
-
                 ])
             })
     )
@@ -31,7 +31,17 @@ self.addEventListener('install', function (event) {
 self.addEventListener('activate', function (event) {
     console.log('%c [Service Worker] Activating Service Worker ...',
         "background:lightblue;color:black;padding:3px;", event);
-    event.waitUntil()
+    event.waitUntil(
+        caches.keys()
+            .then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    if (key !== STATIC_CACHE_NAME && key !== DYNAMIC_CACHE_NAME) {
+                        console.log("%c [Service Worker] Removing old cache.", "background:orange; color:white;padding:3px;", key);
+                        return caches.delete(key)
+                    }
+                }))
+            })
+    )
     return self.clients.claim();
 })
 
@@ -45,7 +55,7 @@ self.addEventListener("fetch", function (event) {
                 } else {
                     return fetch(event.request)
                         .then(function (resp) {
-                            return caches.open("dynamicCache")
+                            return caches.open(DYNAMIC_CACHE_NAME)
                                 .then(function (cache) {
                                     cache.put(event.request.url, resp.clone())
                                     return resp
