7361df7fde7d85de72ce3d97a66bf5b24cf6e283
saeid <saeidebrahimiapply@gmail.com>
2024-10-21 23:23:50 +0330

    add better cache approach in pwa

---
+++ 17.PWA/05.firstApp-with-advance-cache/public/src/js/app.js
@@ -0,0 +1,90 @@
+var deferredPrompt;
+if (!window.Promise) {
+    window.Promise = Promise
+}
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        // .register('/sw.js', { scope: '/help' })
+        .then(function () {
+            console.log("%c service worker registered!", "background:blue;color:white;padding:3px;");
+        }).catch(function (error) {
+            console.warn(error)
+        });
+}
+window.addEventListener('beforeinstallprompt', function (evt) {
+    console.log("%c beforeinstallprompt fired", 'background:yellow;color:black;padding:5px;');
+    evt.preventDefault();
+    deferredPrompt = evt;
+    return false
+})
+// var promise = new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//         // resolve("This is executed once the timer is done!")
+//         reject({ code: 500, message: "An error occurred!" })
+//         // console.log("This is executed once the timer is done!");
+//     }, 3000);
+// });
+// XHR is a synchronous  operation and service worker don't support it.
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://httpbin.org/ip");
+// xhr.responseType = "json";
+// xhr.onload = function () {
+//     console.log(xhr.response);
+// }
+// xhr.onerror = function () {
+//     console.log("Error!");
+// }
+// xhr.send()
+
+// fetch("https://httpbin.org/ip")
+//     .then(function (response) {
+//         console.log(response);
+//         return response.json()
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
+
+
+// const postData = {
+//     message: "Does it work?"
+// }
+// fetch("https://httpbin.org/post", {
+//     method: "POST",
+//     headers: {
+//         'Content-Type': "application/json",
+//         "Accept": "application/json"
+//     },
+//     mode: "cors",
+//     // mode: "no-cors",
+//     body: JSON.stringify(postData)
+// })
+//     .then(function (response) {
+//         console.log(response);
+//         return response.json()
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
+// promise.then(function (text) {
+//     console.log(text);
+// }, function (error) {
+//     console.warn(`${error.code}: ${error.message}`);
+// }).then(function (newText) {
+//     console.log(newText);
+// })
+// promise.then(function (text) {
+//     console.log(text);
+// }).then(function (newText) {
+//     console.log(newText);
+// }).catch(error => {
+//     console.error(`${error.code}: ${error.message}`);
+// })
+// console.log("This is executed right after setTimeout()");
---
+++ 17.PWA/05.firstApp-with-advance-cache/public/sw.js
@@ -0,0 +1,152 @@
+var STATIC_CACHE_NAME = 'static-v14'
+var DYNAMIC_CACHE_NAME = "dynamic-v2"
+// service worker scope is depends on the folder which exists
+self.addEventListener('install', function (event) {
+    console.log('%c [Service Worker] Installing Service Worker ...', "background:green; color:white;padding:3px;", event);
+    event.waitUntil(
+        caches.open(STATIC_CACHE_NAME)
+            .then(function (cache) {
+                console.log("[Service Worker] Pre Caching App Shell");
+                // cache.add("/")
+                cache.addAll([
+                    "/",
+                    "/index.html",
+                    "/offline.html",
+                    "src/js/app.js",
+                    "src/js/feed.js",
+                    "src/js/material.min.js",
+                    "src/css/app.css",
+                    "src/css/feed.css",
+                    "/favicon.ico",
+                    "src/images/main-image.jpg",
+                    "https://fonts.googleapis.com/css?family=Roboto:400,700",
+                    "https://fonts.googleapis.com/icon?family=Material+Icons",
+                    "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+                    // adding polyfills are unnecessary
+                    // because old browser don't support service workers "src/js/promise.js", "src/js/fetch.js"
+                ])
+            })
+    )
+})
+
+self.addEventListener('activate', function (event) {
+    console.log('%c [Service Worker] Activating Service Worker ...',
+        "background:lightblue;color:black;padding:3px;", event);
+    event.waitUntil(
+        caches.keys()
+            .then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    if (key !== STATIC_CACHE_NAME && key !== DYNAMIC_CACHE_NAME) {
+                        console.log("%c [Service Worker] Removing old cache.", "background:orange; color:white;padding:3px;", key);
+                        return caches.delete(key)
+                    }
+                }))
+            })
+    )
+    return self.clients.claim();
+})
+// best approach: first cache then update cache with fetch result
+self.addEventListener("fetch", function (event) {
+    var url = 'https://httpbin.org/get'
+    if (event.request.url.indexOf(url) > -1) {
+        event.respondWith(
+            caches.open(DYNAMIC_CACHE_NAME)
+                .then(function (cache) {
+                    return fetch(event.request)
+                        .then(function (resp) {
+                            cache.put(event.request, resp.clone())
+                            return resp
+                        })
+                })
+        )
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then(function (response) {
+                    if (response) {
+                        return response;
+                    } else {
+                        return fetch(event.request)
+                            .then(function (resp) {
+                                return caches.open(DYNAMIC_CACHE_NAME)
+                                    .then(function (cache) {
+                                        cache.put(event.request.url, resp.clone())
+                                        return resp
+                                    })
+                            })
+                            .catch(function (err) {
+                                console.log(err)
+                                return caches.open(STATIC_CACHE_NAME)
+                                    .then(function (cache) {
+                                        if (event.request.url.indexOf("/help") > -1) {
+                                            return cache.match("/offline.html")
+                                        }
+                                    })
+                            })
+                    }
+                })
+        )
+    }
+
+})
+
+// cache first approach
+// self.addEventListener("fetch", function (event) {
+//     // console.log('%c [Service Worker] Fetching something...', "background:gray;text:white;", event);
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(function (response) {
+//                 if (response) {
+//                     return response;
+//                 } else {
+//                     return fetch(event.request)
+//                         .then(function (resp) {
+//                             return caches.open(DYNAMIC_CACHE_NAME)
+//                                 .then(function (cache) {
+//                                     cache.put(event.request.url, resp.clone())
+//                                     return resp
+//                                 })
+//                         })
+//                         .catch(function (err) {
+//                             console.log(err)
+//                             return caches.open(STATIC_CACHE_NAME)
+//                                 .then(function (cache) {
+//                                     return cache.match("/offline.html")
+//                                 })
+//                         })
+//                 }
+//             })
+
+//     )
+// });
+
+// network first approach: bad user experience for slow networks
+// self.addEventListener("fetch", function (event) {
+//     event.respondWith(
+//         fetch(event.request)
+//             .then(function (resp) {
+//                 return caches.open(DYNAMIC_CACHE_NAME)
+//                     .then(function (cache) {
+//                         cache.put(event.request.url, resp.clone())
+//                         return resp
+//                     })
+//             })
+//             .catch(function (error) {
+//                 return caches.match(event.request)
+//             })
+//     )
+// });
+
+// cache only approach: is not recommended
+// self.addEventListener("fetch", function (event) {
+//     event.respondWith(
+//         caches.match(event.request)
+//     )
+// });
+
+// Network only approach: is not recommended
+// self.addEventListener("fetch", function (event) {
+//     event.respondWith(
+//         fetch(event.request)
+//     )
+// })
