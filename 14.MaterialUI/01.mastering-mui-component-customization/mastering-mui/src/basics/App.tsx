7549f632a89032dfd4e6aafb5047b9dbfb4d9f50
saeid <saeidebrahimiapply@gmail.com>
2025-01-14 23:34:00 +0330

    base concepts of mui

---
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/basics/App.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from 'react'
+import { Box, Button } from '@mui/material'
+
+function MySpecialBox(props: { label: string; children: ReactNode }) {
+    return <Box>
+        label: {props.label}
+        {props.children}
+    </Box>
+}
+function App() {
+    return (
+        <div>
+            <Button sx={
+                {
+                    backgroundColor: "darkgreen",
+                }
+            } disableElevation onClick={() => {
+                console.log("clicked")
+            }} variant={"contained"}
+            // component="a"
+            >My Button</Button>
+            <MySpecialBox label="Special Box">
+                <Button variant={"outlined"}>My Box</Button>
+            </MySpecialBox>
+        </div>
+    )
+}
+
+export default App
