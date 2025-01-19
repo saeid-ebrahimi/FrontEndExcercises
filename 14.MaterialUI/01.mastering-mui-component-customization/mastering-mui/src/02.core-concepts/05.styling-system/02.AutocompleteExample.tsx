47c76d3ec6ec4af398eec660dabaad084358f0bf
saeid <saeidebrahimiapply@gmail.com>
2025-01-20 00:50:34 +0330

    using props to customize components

--- 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/02.core-concepts/05.styling-system/02.AutocompleteExample.tsx
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/02.core-concepts/05.styling-system/02.AutocompleteExample.tsx
@@ -1,35 +1,58 @@
-import { Autocomplete, Chip, rgbToHex, TextField } from "@mui/material";
+import { Autocomplete, Chip, Paper, PaperProps, rgbToHex, TextField } from "@mui/material";
+import { PropsWithChildren } from "react";
 
+const CustomPaper = (props: PaperProps) => {
+    const { sx, ...otherProps } = props
+    return (
+        <Paper {...otherProps} sx={{
+            ...sx,
+            bgcolor: "black",
+            color: "white",
+            "& .MuiAutocomplete-option": {
+                fontFamily: "cursive",
+            },
+        }}>
+            {props.children}
+        </Paper >
+    )
+}
 export function FruitsAutocomplete() {
 
     const fruitOptions = ["Apple", "Banana", "Pear", "Orange", "Lemon", "Pineapple"]
     return (
-        <Autocomplete open={true} multiple id="fruits" options={fruitOptions} ChipProps={{
-            sx:
-            {
-                background: "black",
-                "& .MuiChip-label": {
-                    color: "white",
-                    fontFamily: "Verdana",
-                    fontSize: "0.9rem"
-                },
-                "& .MuiChip-deleteIcon": {
-                    color: "white",
-                },
-            }
-        }}
-            ListboxProps={{
-                sx: {
-                    background: "black",
-                    color: "white",
-                    "& .MuiAutocomplete-option": {
-                        "&:hover": {
-                            backgroundColor: "#00cccc"
-                        }
+        <>
+            <Autocomplete
+                multiple
+                id="fruits"
+                options={fruitOptions}
+                ChipProps={{
+                    sx:
+                    {
+                        background: "black",
+                        "& .MuiChip-label": {
+                            color: "white",
+                            fontFamily: "Verdana",
+                            fontSize: "0.9rem"
+                        },
+                        "& .MuiChip-deleteIcon": {
+                            color: "white",
+                        },
                     }
-                }
-            }}
+                }}
+                // ListboxProps={{
+                //     sx: {
+                //         background: "black",
+                //         color: "white",
+                //         "& .MuiAutocomplete-option": {
+                //             "&:hover": {
+                //                 backgroundColor: "#00cccc"
+                //             }
+                //         }
+                //     }
+                // }}
+                PaperComponent={CustomPaper}
+                renderInput={(params) => <TextField {...params} label="Fruit" placeholder={"Favorite Fruits..."} />} />
 
-            renderInput={(params) => <TextField {...params} label="Fruit" placeholder={"Favorite Fruits..."} />} />
+        </>
     )
 }
