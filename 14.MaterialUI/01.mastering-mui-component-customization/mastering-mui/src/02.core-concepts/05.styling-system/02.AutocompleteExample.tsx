120b138fa1fea8a5fd09b3391582de1654203471
saeid <saeidebrahimiapply@gmail.com>
2025-01-19 23:58:41 +0330

    adding styleing components in mui

--- 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/02.core-concepts/05.styling-system/02.AutocompleteExample.tsx
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/02.core-concepts/05.styling-system/02.AutocompleteExample.tsx
@@ -1,9 +1,35 @@
-import { Autocomplete, Chip, TextField } from "@mui/material";
+import { Autocomplete, Chip, rgbToHex, TextField } from "@mui/material";
 
 export function FruitsAutocomplete() {
 
     const fruitOptions = ["Apple", "Banana", "Pear", "Orange", "Lemon", "Pineapple"]
     return (
-        <Autocomplete multiple id="fruits" options={fruitOptions} renderInput={(params) => <TextField {...params} label="Fruit" placeholder={"Favorite Fruits..."} />} />
+        <Autocomplete open={true} multiple id="fruits" options={fruitOptions} ChipProps={{
+            sx:
+            {
+                background: "black",
+                "& .MuiChip-label": {
+                    color: "white",
+                    fontFamily: "Verdana",
+                    fontSize: "0.9rem"
+                },
+                "& .MuiChip-deleteIcon": {
+                    color: "white",
+                },
+            }
+        }}
+            ListboxProps={{
+                sx: {
+                    background: "black",
+                    color: "white",
+                    "& .MuiAutocomplete-option": {
+                        "&:hover": {
+                            backgroundColor: "#00cccc"
+                        }
+                    }
+                }
+            }}
+
+            renderInput={(params) => <TextField {...params} label="Fruit" placeholder={"Favorite Fruits..."} />} />
     )
 }
