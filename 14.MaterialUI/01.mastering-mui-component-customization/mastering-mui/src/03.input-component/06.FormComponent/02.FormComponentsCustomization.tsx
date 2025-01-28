1d0f7c1319e355c340b732b2aace182d9048bfcc
saeid <saeidebrahimiapply@gmail.com>
2025-01-29 00:25:02 +0330

    form control style customization using classes object

--- 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/03.input-component/06.FormComponent/02.FormComponentsCustomization.tsx
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/03.input-component/06.FormComponent/02.FormComponentsCustomization.tsx
@@ -1,8 +1,12 @@
-import { Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel } from "@mui/material";
+import {
+    Checkbox, FormControl, FormControlLabel, FormGroup,
+    FormHelperText, FormLabel, formControlLabelClasses,
+    formLabelClasses, formHelperTextClasses
+} from "@mui/material";
 
 export function FormComponentsCustomization() {
     return (
-        <div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
             <FormControl
                 // required
                 component={"fieldset"}
@@ -94,6 +98,57 @@ export function FormComponentsCustomization() {
                         //     color: "#94A3B8"
                         // }
                     }}
+                >Please select your favorite movies</FormHelperText>
+            </FormControl>
+            <FormControl
+                // required
+                component={"fieldset"}
+                margin={"dense"}
+                sx={{
+                    [`& .${formControlLabelClasses.label}`]: {
+                        color: "#1E293B",
+                        fontSize: "1.2rem",
+                        [`&.${formControlLabelClasses.disabled}`]: {
+                            color: "#94A3B8"
+                        }
+                    },
+                    [`& .${formLabelClasses.root}`]: {
+                        color: "#4338CA",
+                        fontSize: "1.5rem",
+                        fontFamily: "Verdana",
+                    },
+                    [`& .${formLabelClasses.focused}`]: {
+                        color: "#16A34A",
+                    },
+                    [`& .${formLabelClasses.asterisk}`]: {
+                        color: "red",
+                    },
+                    [`& .${formHelperTextClasses.root}`]: {
+                        color: "#4338CA",
+                        fontFamily: "verdana",
+                    },
+                    [`& .${formControlLabelClasses.error}`]: {
+                        color: "#C2410C"
+                    },
+                    [`& .${formControlLabelClasses.disabled}`]: {
+                        color: "#94A3B8",
+                        [`& .${formLabelClasses.asterisk}`]: {
+                            color: "#94A3B8",
+                        }
+                    },
+
+                }}
+                required
+            // error
+            // disabled
+            >
+                <FormLabel>My Favorite Movies</FormLabel>
+                <FormGroup >
+                    <FormControlLabel control={<Checkbox />} label={"Save Private Ryan"} />
+                    <FormControlLabel control={<Checkbox />} label={"The God Father"} />
+                    <FormControlLabel control={<Checkbox />} label={"The Oppenheimer"} />
+                </FormGroup>
+                <FormHelperText
                 >Please select your favorite movies</FormHelperText>
             </FormControl>
         </div >
