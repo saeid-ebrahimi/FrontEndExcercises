ac8e8f9a3b43ad6494ed23c408043a6c83122622
saeid <saeidebrahimiapply@gmail.com>
2025-01-31 13:17:23 +0330

    add app file to rating folder

---
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/03.input-component/08.Rating/App.tsx
@@ -0,0 +1,17 @@
+import { RatingFunctionality } from "./01.RatingFunctionality";
+import { RatingCustomization } from "./02.RatingCustomization";
+import { RatingCustomizationUsingClassesObject } from "./03.RatingCustomizationUsingClassesObject";
+
+export default function App() {
+    return <div style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        gap: "2rem"
+    }}>
+        <RatingFunctionality />
+        <RatingCustomization />
+        <RatingCustomizationUsingClassesObject />
+    </div>
+}
