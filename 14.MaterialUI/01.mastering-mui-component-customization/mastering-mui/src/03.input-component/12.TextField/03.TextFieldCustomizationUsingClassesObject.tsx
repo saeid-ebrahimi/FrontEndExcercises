0ac62c7ab735ecdc8d8cdbf17d28a819e8da694c
saeid <saeidebrahimiapply@gmail.com>
2025-02-08 23:52:56 +0330

    add input customization using classes object for standard text field

--- 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/03.input-component/12.TextField/03.TextFieldCustomizationUsingClassesObject.tsx
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/03.input-component/12.TextField/03.TextFieldCustomizationUsingClassesObject.tsx
@@ -139,20 +139,116 @@ export default function TextFieldCustomizationUsingClassesObject() {
                     }
                 }}
             />
-            {/* <TextField
+            <TextField
                 value={phoneNumber}
                 label={"Phone Number"}
                 variant={"standard"}
                 autoComplete="off"
                 type={visible ? "number" : "password"}
+                required
+                // disabled
+                error={phoneNumber.length !== 11 && phoneNumber.length > 0}
+                helperText={phoneNumber.length !== 11 && phoneNumber.length > 0 ? "your phone number should be 11 digits" : "enter your emergency phone number"}
+                placeholder={"Enter Your Phone Number..."}
+                margin={"normal"}
+                onChange={
+                    (event: ChangeEvent<HTMLInputElement>) => {
+                        setPhoneNumber(event.target.value);
+                    }
+                }
                 id={"phone-number"}
                 sx={{
                     "& input[type=number]::-webkit-inner-spin-button,input[type=number]::-webkit-outer-spin-button": {
                         appearance: "none",
                     },
+                    [`& .${outlinedInputClasses.error}`]: {
+                        [`& .${inputLabelClasses.root}`]: {
+                            color: "#C2410C"
+                        },
+                        [`& .${inputLabelClasses.asterisk}`]: {
+                            color: "#C2410C"
+                        },
+                        [`&.${formHelperTextClasses.root}`]: {
+                            color: "#C2410C"
+                        },
+                        [`&.${inputLabelClasses.shrink}`]: {
+                            color: "#C2410C"
+                        }
+                    },
+                    [`& .${outlinedInputClasses.disabled}`]: {
+                        [`& .${inputLabelClasses.root}`]: {
+                            color: "#D8B4FE",
+                        },
+                        [`&.${formHelperTextClasses.root}`]: {
+                            color: "#D8B4FE",
+                        },
+                        "&::placeholder": {
+                            color: "red !important"
+                        }
+                    },
+                    [`& .${inputLabelClasses.root}`]: {
+                        color: "#0E7490",
+                        fontFamily: "Verdana",
+                    },
+                    [`& .${inputLabelClasses.shrink}`]: {
+                        color: "#4C1D95",
+                    },
+                    "& ::placeholder": {
+                        color: "#4C1D95",
+                        opacity: 0.8,
+                    },
+                    [`& .${formHelperTextClasses.root}`]: {
+                        color: "#4C1D95",
+                        fontFamily: "Verdana",
+                        fontSize: "0.75rem",
+                    },
 
                 }}
-            /> */}
+
+                slotProps={{
+                    input: {
+                        // readOnly: true,
+                        sx: {
+                            color: "#4C1D95",
+                            fontFamily: "Verdana",
+                            height: "4rem",
+                            "&.Mui-error": {
+                                bgcolor: "#FFF7ED",
+                                color: "#C2410C"
+                            },
+                            "&.Mui-disabled": {
+                                bgcolor: "white",
+                            },
+                            "&:before": {
+                                borderColor: "#4C1D95",
+
+                            },
+                            "&:after": {
+                                borderColor: "#4C1D95",
+                            },
+                            "&:not(.Mui-disabled):hover::before": {
+                                borderColor: "#4C1D95",
+                            }
+
+                        },
+                        startAdornment:
+                            <InputAdornment sx={{
+                                "& > svg": {
+                                    color: "#4C1D95",
+                                },
+                            }} position={"start"}>
+                                <PhoneIcon />
+                            </InputAdornment>,
+                        endAdornment:
+                            <InputAdornment onClick={() => { setVisible(prev => !prev) }} position={"end"}>
+                                {visible ? <VisibilityOffIcon sx={{
+                                    color: "#6B21A8",
+                                }} /> : <VisibilityIcon sx={{ color: "#4C1D95" }} />}
+                            </InputAdornment>
+                    }
+                }}
+
+            />
         </>
     )
 }
