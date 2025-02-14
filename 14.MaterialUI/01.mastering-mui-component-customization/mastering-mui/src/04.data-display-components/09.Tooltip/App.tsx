6477cb4d65b42c2b2b6dbf443f2c2ab88ccdd019
saeid <saeidebrahimiapply@gmail.com>
2025-02-15 00:35:28 +0330

    add app file to tooltip folder

---
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/04.data-display-components/09.Tooltip/App.tsx
@@ -0,0 +1,16 @@
+import { TooltipFunctionality } from "./01.TooltipFunctionality";
+import { TooltipCustomization } from "./02.TooltipCustomization";
+
+export default function App() {
+    return <div style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        minHeight: "100vh",
+        alignItems: "center",
+        gap: "2rem"
+    }}>
+        <TooltipFunctionality />
+        <TooltipCustomization />
+    </div>
+}
