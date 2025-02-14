83053e34bdbbf32dfc84fbd5b9bf23327d46f386
saeid <saeidebrahimiapply@gmail.com>
2025-02-14 13:21:41 +0330

    add app to list folder

--- 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/04.data-display-components/05.List/App.tsx
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/04.data-display-components/05.List/App.tsx
@@ -1,6 +1,6 @@
-import { ListFunctionality } from "./05.List/01.ListFunctionality";
-import { ListCustomization } from "./05.List/02.ListCustomization";
-import { ListCustomizationUsingClassesObject } from "./05.List/03.ListCustomizationUsingClassesObject";
+import { ListFunctionality } from "./01.ListFunctionality";
+import { ListCustomization } from "./02.ListCustomization";
+import { ListCustomizationUsingClassesObject } from "./03.ListCustomizationUsingClassesObject";
 
 export default function App() {
     return <div style={{
