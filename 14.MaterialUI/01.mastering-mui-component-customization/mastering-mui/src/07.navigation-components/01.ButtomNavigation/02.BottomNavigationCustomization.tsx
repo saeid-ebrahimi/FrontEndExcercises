178e7400856a226de991d6642ad693d5ba60ef24
saeid <saeidebrahimiapply@gmail.com>
2025-02-28 20:55:36 +0330

    add customization

---
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/07.navigation-components/01.ButtomNavigation/02.BottomNavigationCustomization.tsx
@@ -0,0 +1,80 @@
+import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
+import {
+    AccountBalance as AccountBalanceIcon,
+    AccountCircle as AccountCircleIcon,
+    AddCard as AddCardIcon,
+    AddLocation as AddLocationIcon,
+    AddIcCall as AddIcCallIcon
+} from "@mui/icons-material";
+import { SyntheticEvent, useState } from "react";
+
+export function BottomNavigationCustomization() {
+    const [selected, setSelected] = useState("balance")
+
+    const handleChange = (_event: SyntheticEvent, newValue: string) => {
+        setSelected(newValue)
+    }
+    return (
+        <>
+            <Paper sx={{
+                position: "fixed",
+                bottom: 0,
+                left: 0,
+                right: 0,
+            }}>
+                <BottomNavigation
+                    showLabels
+                    value={selected}
+                    onChange={handleChange}
+                    sx={{
+                        bgcolor: "#334155",
+                        height: "4rem",
+                        "& button:not(.Mui-selected)": {
+                            color: "#94A3B8",
+                            gap: "5px",
+                            "& .MuiBottomNavigationAction-label": {
+                                color: "#C084FC"
+                            }
+                        },
+                        "& button.Mui-selected": {
+                            color: "#F8FAFC",
+                            gap: "3px",
+                            transform: "scale(1.08)",
+                            "& .MuiBottomNavigationAction-label": {
+                                color: "#F5D0FE",
+                                fontWeight: 600
+                            }
+                        }
+                    }}
+                >
+                    <BottomNavigationAction
+                        label={"Balance"}
+                        value={"balance"}
+                        icon={<AccountBalanceIcon />}
+                    />
+                    <BottomNavigationAction
+                        label={"Account"}
+                        value={"account"}
+                        icon={<AccountCircleIcon />}
+
+                    />
+                    <BottomNavigationAction
+                        label={"Card"}
+                        value={"card"}
+                        icon={<AddCardIcon />}
+                    />
+                    <BottomNavigationAction
+                        label={"Location"}
+                        value={"location"}
+                        icon={<AddLocationIcon />}
+                    />
+                    <BottomNavigationAction
+                        label={"Call"}
+                        value={"call"}
+                        icon={<AddIcCallIcon />}
+                    />
+                </BottomNavigation>
+            </Paper>
+        </>
+    )
+}
