ddd1accf20f5e5034671ef6f3b22b13f391e54ce
saeid <saeidebrahimiapply@gmail.com>
2025-02-28 14:56:47 +0330

    add app to paper folder

---
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/06.surface-components/04.Paper/App.tsx
@@ -0,0 +1,19 @@
+import { Divider } from "@mui/material";
+import { PaperFunctionality } from "./01.PaperFunctionality";
+import { PaperCustomization } from "./02.PaperCustomization";
+
+export default function App() {
+    return <div style={{
+        display: "flex",
+        flexDirection: "column",
+        // justifyContent: "center",
+        margin: 20,
+        minHeight: "100vh",
+        alignItems: 'normal',
+        gap: "1.5rem"
+    }}>
+        <PaperFunctionality />
+        <Divider flexItem variant={"middle"}>Customization</Divider>
+        <PaperCustomization />
+    </div>
+}
