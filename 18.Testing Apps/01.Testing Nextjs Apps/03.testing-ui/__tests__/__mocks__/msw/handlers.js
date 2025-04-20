c75918976bf2b89ff159451e38d5200bebb9284a
saeid <saeidebrahimiapply@gmail.com>
2025-04-21 00:25:53 +0330

    write handler for the msx show route

--- 18.Testing Apps/01.Testing Nextjs Apps/03.testing-ui/__tests__/__mocks__/msw/handlers.js
+++ 18.Testing Apps/01.Testing Nextjs Apps/03.testing-ui/__tests__/__mocks__/msw/handlers.js
@@ -1,9 +1,13 @@
 import { http, HttpResponse } from "msw";
+import { readFakeData } from "../fakeData";
+
 
 export const handlers = [
-    http.get("http://", () => {
-        return HttpResponse.json({
 
+    http.get("http://localhost:3000/api/shows/:showId", async () => {
+        const { fakeShows } = await readFakeData()
+        return HttpResponse.json({
+            show: fakeShows[0]
         })
     })
 ]
