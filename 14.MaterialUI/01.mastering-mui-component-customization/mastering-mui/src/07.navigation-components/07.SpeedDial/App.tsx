fd396988bc8354eba9f3d82807c0e0e141243f4f
saeid <saeidebrahimiapply@gmail.com>
2025-05-01 13:58:28 +0330

    add app file to speed dial

---
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/07.navigation-components/07.SpeedDial/App.tsx
@@ -0,0 +1,18 @@
+import { Divider } from "@mui/material";
+import SpeedDialFunctionality from "./01.SpeedDialFunctionality";
+import SpeedDialCustomization from "./02.SpeedDialCustomization";
+
+export default function App() {
+    return <div style={{
+        flexDirection: "column",
+        // justifyContent: "center",
+        margin: 20,
+        minHeight: "100vh",
+        alignItems: 'normal',
+        gap: "1.5rem"
+    }}>
+        <SpeedDialFunctionality />
+        <Divider flexItem variant={"middle"}>Customization</Divider>
+        <SpeedDialCustomization />
+    </div>
+}
