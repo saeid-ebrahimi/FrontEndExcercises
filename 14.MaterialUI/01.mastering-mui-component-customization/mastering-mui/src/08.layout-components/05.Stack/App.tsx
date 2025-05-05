b000c7e4170695afb4d2fa858d25db6b108c951f
saeid <saeidebrahimiapply@gmail.com>
2025-05-06 00:57:10 +0330

    add app file to stack folder

---
+++ 14.MaterialUI/01.mastering-mui-component-customization/mastering-mui/src/08.layout-components/05.Stack/App.tsx
@@ -0,0 +1,20 @@
+import { Divider } from "@mui/material";
+import StackFunctionality from "./01.StackFunctionality";
+import StackCustomization from "./02.StackCustomization";
+
+
+export default function App() {
+    return <div style={{
+        flexDirection: "column",
+        // justifyContent: "center",
+        margin: 20,
+        minHeight: "100vh",
+        alignItems: 'normal',
+        gap: "1.5rem"
+    }}>
+        <StackFunctionality />
+        <Divider flexItem variant={"middle"}>Customization</Divider>
+        <StackCustomization />
+
+    </div>
+}
