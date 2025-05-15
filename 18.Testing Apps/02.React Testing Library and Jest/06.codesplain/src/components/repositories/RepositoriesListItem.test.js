37e90c7e9a996d04e1f4e1cb0e57803a3f8886ba
saeid <saeidebrahimiapply@gmail.com>
2025-05-15 23:30:24 +0330

    add resolve act warning

--- 18.Testing Apps/02.React Testing Library and Jest/06.codesplain/src/components/repositories/RepositoriesListItem.test.js
+++ 18.Testing Apps/02.React Testing Library and Jest/06.codesplain/src/components/repositories/RepositoriesListItem.test.js
@@ -1,15 +1,22 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, act } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import RepositoriesListItem from "./RepositoriesListItem"
+// make module mock
+// jest.mock('../tree/FileIcon.js', () => {
+//     // content of FileIcon.js
+//     return () => {
+//         return "File Icon Component"
+//     }
+// })
 
 function renderComponent() {
     const repository = {
         full_name: "facebook/react",
-        language: "C++",
-        description: "A framework for building native applications using React",
+        language: "Javascript",
+        description: "A framework for building applications using React",
         owner: "facebook",
-        name: "react-native",
-        html_url: "https://github.com/facebook/react-native",
+        name: "react",
+        html_url: "https://github.com/facebook/react",
     }
     render(<MemoryRouter>
         <RepositoriesListItem repository={repository} />
@@ -17,8 +24,37 @@ function renderComponent() {
 }
 
 
-test("shows a link to the github homepage for this repository", () => {
+test("shows a link to the github homepage for this repository", async () => {
     renderComponent()
 
-    const linkElement = screen
+    // debug
+    // screen.debug();
+    // await pause();
+    // screen.debug();
+
+
+    // await screen.findByRole("img", { name: "Javascript" })
+
+    // screen.debug()
+
+    // the last and worst option
+    await act(async () => {
+        await pause()
+    })
+
 })
+// debug pause function
+// const pause = () => {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve();
+//         }, 100)
+//     })
+// }
+
+// act pause function 
+const pause = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 100)
+    })
+}
