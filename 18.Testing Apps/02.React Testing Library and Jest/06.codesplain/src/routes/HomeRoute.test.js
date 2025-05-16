3d3f9323b185709003e6c00e9ca4081cfa88520f
saeid <saeidebrahimiapply@gmail.com>
2025-05-16 20:34:17 +0330

    add home route msw setup

--- 18.Testing Apps/02.React Testing Library and Jest/06.codesplain/src/routes/HomeRoute.test.js
+++ 18.Testing Apps/02.React Testing Library and Jest/06.codesplain/src/routes/HomeRoute.test.js
@@ -5,18 +5,20 @@ import HomeRoute from "./HomeRoute"
 import { MemoryRouter } from "react-router";
 
 "/api/repositories?q=stars:%3E10000+language:javascript&per_page=10"
-const handlers = rest.get("/api/repositories", (req, res, ctx) => {
-  req.url.searchParams.get('q');
-  console.log(query);
-  return res(
-    ctx.json({
-      items: [
-        { id: 1, full_name: "full name!" },
-        { id: 2, full_name: "other name!" }
-      ]
-    })
-  )
-})
+const handlers = [
+  rest.get("/api/repositories", (req, res, ctx) => {
+    req.url.searchParams.get('q');
+    console.log(query);
+    return res(
+      ctx.json({
+        items: [
+          { id: 1, full_name: "full name!" },
+          { id: 2, full_name: "other name!" }
+        ]
+      })
+    )
+  })
+]
 
 const server = setupServer(...handlers);
 beforeAll(() => {
