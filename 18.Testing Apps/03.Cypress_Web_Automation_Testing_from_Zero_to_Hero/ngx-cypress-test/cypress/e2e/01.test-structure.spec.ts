b5223a78e8f14e049bf00b5796247a7ecdc8de8a
saeid <saeidebrahimiapply@gmail.com>
2025-05-19 00:05:55 +0330

    add test structure file cypress

---
+++ 18.Testing Apps/03.Cypress_Web_Automation_Testing_from_Zero_to_Hero/ngx-cypress-test/cypress/e2e/01.test-structure.spec.ts
@@ -0,0 +1,41 @@
+describe("first test suite", () => {
+
+    describe("suite section", () => {
+
+        beforeEach("login",() => {
+
+        })
+        
+        it("first test", () => {
+            //put the test code 
+        })
+
+        it("second test", () => {
+            //put the test code 
+        })
+
+        it("third test", () => {
+            //put the test code 
+        })
+    })
+})
+
+
+describe("second test suite", () => {
+
+    it('first test', () => {
+        //put the test code 
+    })
+
+    it('second test', () => {
+        //put the test code 
+    })
+
+    it('third test', () => {
+        //put the test code 
+    })
+
+    it('forth test', () => {
+        //put the test code 
+    })
+})
