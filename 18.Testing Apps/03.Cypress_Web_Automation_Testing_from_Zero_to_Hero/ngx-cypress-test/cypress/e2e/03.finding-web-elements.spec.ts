26699bcb2de0cc176281471b393b3ea4615d6c52
saeid <saeidebrahimiapply@gmail.com>
2025-05-19 00:08:54 +0330

    finding web elements in cypress

---
+++ 18.Testing Apps/03.Cypress_Web_Automation_Testing_from_Zero_to_Hero/ngx-cypress-test/cypress/e2e/03.finding-web-elements.spec.ts
@@ -0,0 +1,30 @@
+
+import { navigateToForm } from "./02.types-of-locators.spec"
+
+describe("First test suite", () => {
+    
+    it("second test", () => {
+        navigateToForm()
+
+        // Theory
+        // get() -> find elements on the page by locators globally
+        // find() -> find child element by locators
+        // contains() -> find first HTML text and by text and locator
+
+        cy.contains(/sign in/i)
+        cy.contains('[status="warning"]', /sign in/i)
+        cy.contains('nb-card',"Horizontal form").find("button")
+        cy.contains('nb-card',"Horizontal form").contains("Sign in")
+        cy.contains('nb-card',"Horizontal form").get("button")   // finds all buttons in current page
+
+        // cypress chains and DOM
+        cy.get('#inputEmail3')
+            .parents("form")
+            .find("button")
+            .should('contain', "Sign in")
+            .parents("forms")
+            .find("nb-checkbox")
+            .click()
+
+    })
+})
