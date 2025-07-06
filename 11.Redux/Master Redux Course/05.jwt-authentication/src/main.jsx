c2d2a755d2b37d7a7ccd71e5be9ed7ce72a2f751
saeid <saeidebrahimiapply@gmail.com>
2025-07-06 22:04:32 +0330

    add routes and initialize react router dom

--- 11.Redux/Master Redux Course/05.jwt-authentication/src/main.jsx
+++ 11.Redux/Master Redux Course/05.jwt-authentication/src/main.jsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RegisterForm from './components/RegisterForm';
+import LoginForm from './components/LoginForm';
+import PostForm from './components/PostForm';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/register",
+    element: <RegisterForm />
+  },
+  {
+    path: "/login",
+    element: <LoginForm />
+  },
+  {
+    path: "/post",
+    element: <PostForm />
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
