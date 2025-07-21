46b09ec200b84c2d4653f1c6cd1321909c3e8db8
saeid <saeidebrahimiapply@gmail.com>
2025-07-21 23:49:07 +0330

    complete register form design

--- 11.Redux/Master Redux Course/05.jwt-authentication/src/components/RegisterForm.jsx
+++ 11.Redux/Master Redux Course/05.jwt-authentication/src/components/RegisterForm.jsx
@@ -5,6 +5,14 @@ export default function RegisterForm() {
     return (
         <Form className={"mt-3"}>
             <FormGroup className={"mb-3"} controlId={"loginForm"}>
+                <FormLabel>First Name:</FormLabel>
+                <FormControl type={"text"} placeholder={"Enter First Name"} />
+            </FormGroup>
+            <FormGroup className={"mb-3"} controlId={"loginForm"}>
+                <FormLabel>Last Name:</FormLabel>
+                <FormControl type={"text"} placeholder={"Enter Last Name"} />
+            </FormGroup>
+            <FormGroup className={"mb-3"} controlId={"loginForm"}>
                 <FormLabel>Email Address:</FormLabel>
                 <FormControl type={"email"} placeholder={"Enter Email"} />
                 <FormText>we'll never share your email with anyone else</FormText>
