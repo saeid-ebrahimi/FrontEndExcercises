059781fb660612d37094fe2ad74c9a2158ab6e3d
saeid <saeidebrahimiapply@gmail.com>
2025-07-24 00:27:21 +0330

    add load post using authentication

--- 11.Redux/Master Redux Course/05.jwt-authentication/src/components/Post.jsx
+++ 11.Redux/Master Redux Course/05.jwt-authentication/src/components/Post.jsx
@@ -3,10 +3,23 @@ import { Table } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default function Post() {
+    const [posts, setPosts] = useState()
+    async function getUserPosts() {
+        const accessToken = localStorage.getItem("access_token")
+        const response = await axios.get("http://localhost:3000/api/v1/posts", {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+        setPosts(response.data)
+    }
+    useEffect(() => {
+        getUserPosts()
+    }, [])
     return (
         <div>
-            < to={"/create-post"} title={"Create Post "} className={"btn btn-primary"} >
-            <Table striped bordered hover>
+            <Link to={"/create-post"} title={"Create Post "} className={"btn btn-primary"} >Create Post</Link>
+            <Table className={"mt-3"} striped bordered hover>
                 <thead>
                     <tr>
                         <th>#</th>
@@ -15,11 +28,11 @@ export default function Post() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Test Title</td>
-                        <td>Text Description</td>
-                    </tr>
+                    {posts && posts.length > 0 && posts.map((post, index) => <tr key={index + 1}>
+                        <td>{index + 1}</td>
+                        <td>{post.title}</td>
+                        <td>{post.description}</td>
+                    </tr>)}
                 </tbody>
             </Table>
         </div>
