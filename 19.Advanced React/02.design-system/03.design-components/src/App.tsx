615664e2c51d3f6a9f386272a99eeacee42f9f62
saeid <saeidebrahimiapply@gmail.com>
2025-08-24 01:03:03 +0330

    add modal component

--- 19.Advanced React/02.design-system/03.design-components/src/App.tsx
+++ 19.Advanced React/02.design-system/03.design-components/src/App.tsx
@@ -3,6 +3,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css'
 import { Button } from './components/button';
+import { Modal } from './components/modal';
 
 const queryClient = new QueryClient()
 
@@ -13,6 +14,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <h1>Lets Build Some Components</h1>
         <Button className={"large warning"}>Click Here</Button>
+        <Modal />
       </QueryClientProvider >
 
 
