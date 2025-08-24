ffdbf73ec9b4953016ea9c1e7838b3553edf5d1f
saeid <saeidebrahimiapply@gmail.com>
2025-08-24 19:58:31 +0330

    Design system patterns for spacing

---
+++ 19.Advanced React/02.design-system/03.design-components - Copy/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/users": {
+        target: "http://localhost:9090",
+        changeOrigin: true,
+      },
+      "/current-user": {
+        target: "http://localhost:9090",
+        changeOrigin: true,
+      },
+      "/books": {
+        target: "http://localhost:9090",
+        changeOrigin: true,
+      },
+    },
+  },
+});
