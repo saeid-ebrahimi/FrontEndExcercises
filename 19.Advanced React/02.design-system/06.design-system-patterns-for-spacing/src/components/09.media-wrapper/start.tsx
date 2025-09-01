d8c01842e00af1a3cd04f51aeacf2c6a516e91cf
saeid <saeidebrahimiapply@gmail.com>
2025-09-01 23:57:51 +0330

    fix media wrapper bug

--- 19.Advanced React/02.design-system/06.design-system-patterns-for-spacing/src/components/09.media-wrapper/start.tsx
+++ 19.Advanced React/02.design-system/06.design-system-patterns-for-spacing/src/components/09.media-wrapper/start.tsx
@@ -3,24 +3,24 @@ import tshirt from "../../assets/tshirt.jpg";
 import { Grid } from "../04.grid-pattern/start";
 import { Description } from "./components";
 interface IMediaWrapperProps { ratio: [number, number] }
-export const MediaWrapper = styled.div<IMediaWrapperProps>`
-    position: relative;
-    
+export const MediaWrapper = styled.div<IMediaWrapperProps>`   
     --n: ${(props) => (props.ratio ? props.ratio[0] : 1)};
     --d: ${(props) => (props.ratio ? props.ratio[1] : 1)};
+
     ${(props) => props.ratio && `
         aspect-ratio: var(--n) / var(--d);
-        @supports not (aspect-ration: 1/1){
+        @supports not (aspect-ratio: 1/1){
         padding-block-end: calc(var(--d)/var(--n) * 100%);
     }
     `}
-
+    position: relative;
     > * {
         position: absolute;
         top:0;
         bottom: 0;
         left: 0;
         right:0;
+
         overflow: hidden;
         display: flex;
         justify-content: center;
@@ -37,8 +37,8 @@ const NewProducts = () => {
     return <div>
         <MediaWrapper ratio={[16, 9]}>
             <img src={tshirt} alt={"image of a T-Shirt"} />
-            <Description>White T-Shirt - $19.99</Description>
         </MediaWrapper>
+        <Description>White T-Shirt - $19.99</Description>
     </div>
 }
 
