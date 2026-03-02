7e82691fb5c72c27ea962166f4ac00cbc2e665f8
saeid <saeidebrahimiapply@gmail.com>
2026-03-03 00:12:08 +0330

    handle simple form using react states

--- 21.React Hook Form Complete Guide/01.food-delivery-form/src/FoodDeliveryForm.tsx
+++ 21.React Hook Form Complete Guide/01.food-delivery-form/src/FoodDeliveryForm.tsx
@@ -1,6 +1,10 @@
-import React, { useState } from 'react'
+import { useState, type ChangeEvent, type SyntheticEvent } from 'react'
 
 type FoodDeliveryFormType = {
+    customerName: string;
+    mobile: string;
+}
+type FoodDeliveryFormErrorType = {
     customerName: string;
     mobile: string;
 }
@@ -10,7 +14,67 @@ export function FoodDeliveryForm() {
         mobile: "",
     })
 
+    const [errors, setErrors] = useState<FoodDeliveryFormErrorType>({
+        customerName: "",
+        mobile: ""
+    })
+
+    const validateFormData = () => {
+        let currentErrors: FoodDeliveryFormErrorType = {
+            customerName: "",
+            mobile: ""
+        }
+
+        if (values.customerName.trim() === "") {
+            currentErrors.customerName = "Customer name is required!"
+        }
+        if (values.mobile.trim() === "") {
+            currentErrors.mobile = "Mobile is required!"
+        }
+        setErrors(currentErrors)
+        return Object.values(currentErrors).every(element => element = "")
+    }
+    const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name: fieldName, value } = e.target;
+        setValues({ ...values, [fieldName]: value })
+    }
+
+    const onSubmit = (evt: SyntheticEvent<HTMLFormElement>) => {
+        evt.preventDefault()
+        if (!validateFormData())
+            return;
+        console.log(evt.target);
+
+    }
+
     return (
-        <div>FoodDeliveryForm</div>
+        <form autoComplete={"off"} onSubmit={onSubmit}>
+            <div className={"form-floating mb-3"}>
+                <input onChange={handleChangeInput}
+                    name={"customerName"}
+                    value={values.customerName}
+                    id={"customerName"}
+                    type={"text"}
+                    className={"form-control"}
+                    placeholder={"John Doe"}
+                />
+
+                <label htmlFor={"customerName"}>Customer Name:</label>
+            </div>
+            <p className={"text-danger"}>{errors.customerName}</p>
+            <div className={"form-floating mb-3"}>
+                <input
+                    onChange={handleChangeInput}
+                    name={"mobile"}
+                    id={"mobile"}
+                    type={"text"}
+                    className={"form-control"}
+                    placeholder={"09xx-xxx-xx-xx"}
+                />
+                <label htmlFor="mobile">Mobile:</label>
+            </div>
+            <p className={"text-danger"}>{errors.mobile}</p>
+            <button type={"submit"} className={"btn btn-primary"}>Submit</button>
+        </form>
     )
 }
