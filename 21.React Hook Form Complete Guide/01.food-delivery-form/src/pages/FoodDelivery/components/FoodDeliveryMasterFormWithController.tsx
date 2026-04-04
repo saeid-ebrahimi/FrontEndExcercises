19fc410275183b90a76155a4e46d79801901272b
saeid <saeidebrahimiapply@gmail.com>
2026-04-04 13:43:23 +0330

    add food delivery master form

---
+++ 21.React Hook Form Complete Guide/01.food-delivery-form/src/pages/FoodDelivery/components/FoodDeliveryMasterFormWithController.tsx
@@ -0,0 +1,141 @@
+import { Controller, useFormContext, useFormState } from "react-hook-form";
+
+import type { TFoodDeliveryMasterFormData } from "../../../types";
+import { FormControl, FormLabel, TextField } from "@mui/material";
+// import { getRenderCount } from "../../../lib/getRenderCount";
+
+// const RenderCount = getRenderCount("FoodDeliveryMasterForm")
+
+export function FoodDeliveryMasterFormWithController() {
+    const { control } = useFormContext<TFoodDeliveryMasterFormData>()
+    const { isSubmitting } = useFormState<TFoodDeliveryMasterFormData>({
+        name: ["orderNumber",
+            "customerName",
+            "mobile",
+            "email",],
+        exact: true,
+    })
+    return (
+        <>
+            {/* <RenderCount /> */}
+            <div className="row mb-4">
+                <div className="col">
+                    <Controller
+                        control={control}
+                        name="orderNumber"
+                        disabled={isSubmitting}
+                        exact={true}
+                        rules={{
+                            required: {
+                                value: true,
+                                message: "order number value is required!"
+                            }
+                        }}
+                        render={({ field, fieldState }) => <>
+                            <FormControl fullWidth error={!!fieldState.error}>
+                                <FormLabel id={"orderNumber"} hidden>Order Number</FormLabel>
+                                <TextField
+                                    placeholder={"Order number"}
+                                    label={"Order Number:"}
+                                    helperText={fieldState.error?.message}
+                                    {...field}
+                                />
+                            </FormControl>
+                        </>}
+                    />
+                </div>
+                <div className={"col"}>
+                    <Controller
+                        control={control}
+                        name="mobile"
+                        exact={true}
+                        disabled={isSubmitting}
+                        rules={{
+                            required: {
+                                value: true,
+                                message: "mobile number is required!"
+                            },
+                            minLength: {
+                                value: 10,
+                                message: "mobile field must be 10 characters"
+                            },
+                            maxLength: {
+                                value: 10,
+                                message: "mobile field must be 10 characters"
+                            }
+                        }}
+                        render={({ field, fieldState }) =>
+                            <>
+                                <FormControl fullWidth error={!!fieldState.error}>
+                                    <FormLabel id={"mobile"} hidden={true}>Mobile</FormLabel>
+                                    <TextField
+                                        label={"mobile"}
+                                        placeholder={"enter mobile number"}
+                                        helperText={fieldState.error?.message}
+                                        {...field}
+                                    />
+                                </FormControl>
+                            </>
+                        }
+                    />
+                </div>
+            </div>
+            <div className={"row mb-4"}>
+                <div className="col">
+                    <Controller
+                        control={control}
+                        name={"customerName"}
+                        disabled={isSubmitting}
+                        exact={true}
+                        rules={{
+                            required: {
+                                value: true,
+                                message: "customer name is required!"
+                            }
+                        }}
+                        render={({ field, fieldState }) => <>
+                            <FormControl fullWidth error={!!fieldState.error}>
+                                <FormLabel id={"customerName"} hidden={true}>Customer Name</FormLabel>
+                                <TextField
+                                    label={"customerName"}
+                                    placeholder={"enter customer name"}
+                                    helperText={fieldState.error?.message}
+                                    {...field}
+                                />
+                            </FormControl>
+                        </>}
+                    />
+                </div>
+                <div className={"col"}>
+                    <Controller
+                        control={control}
+                        name={"email"}
+                        exact={true}
+                        disabled={isSubmitting}
+                        rules={{
+                            pattern: {
+                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                message: "incorrect email format entered"
+                            },
+                            validate: {
+                                notFake: (value) => value !== "email@gmail.com" || "the email is blocked",
+                                notFromBlacklistedDomain: (value) => (!value.endsWith(".ir") && !value.includes("@example")) || "this domain is blocked"
+                            }
+                        }}
+                        render={({ field, fieldState }) => <>
+                            <FormControl fullWidth error={!!fieldState.error}>
+                                <FormLabel id={"email"} hidden={true}>Email</FormLabel>
+                                <TextField
+                                    label={"Email"}
+                                    placeholder={"enter email"}
+                                    helperText={fieldState.error?.message}
+                                    {...field}
+                                />
+                            </FormControl>
+                        </>}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
