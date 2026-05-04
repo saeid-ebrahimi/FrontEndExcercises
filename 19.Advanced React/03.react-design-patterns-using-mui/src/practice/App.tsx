6dd1f882ebf545a9d2b2bdbdec4aa2324bc12b4e
saeid <saeidebrahimiapply@gmail.com>
2026-05-04 13:32:30 +0330

    change style

--- 19.Advanced React/03.react-design-patterns-using-mui/src/practice/App.tsx
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/practice/App.tsx
@@ -1,14 +1,15 @@
 import { Box } from "@mui/material"
 import "../App.css"
-import { StackDemo } from "../01.layout-patterns/08.Stack/stackDemo"
+import { ContainerDemo } from "../01.layout-patterns/07.Full-Screen/02.container"
 
 export default function App() {
     return <Box sx={{
         display: "flex",
+        padding: 0,
         flexDirection: "column",
         gap: "2rem",
-        height: "500px"
+        // height: "500px"
     }}>
-        <StackDemo />
+        <ContainerDemo />
     </Box>
 }
