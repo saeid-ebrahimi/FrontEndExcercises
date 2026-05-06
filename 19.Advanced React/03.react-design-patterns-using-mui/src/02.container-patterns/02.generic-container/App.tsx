64bedf75529a4fb06ccd3ef9be8e0f3599e8db1d
saeid <saeidebrahimiapply@gmail.com>
2026-05-06 13:29:53 +0330

    add usage of them

---
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/02.container-patterns/02.generic-container/App.tsx
@@ -0,0 +1,51 @@
+import "../App.css"
+import { TUser, UserInfo } from "./user-info"
+import { ResourceLoader } from "./resource-loader"
+import { BookInfo, TBook } from "./book-info"
+import { DataSource } from "./data-source"
+import axios from "axios"
+import { DataSourceWithRender } from "./data-source-with-render"
+
+async function getDataFromServer<T>(url: string): Promise<T> {
+    // try {
+    const response = await axios.get(url);
+    return response.data
+    // } catch (error) {
+    //     // convert error to readable
+    //     // convertError(error)
+    //     // throw new Error(error.message)
+    // }
+}
+
+const getDataFromLocalStorage = <T,>(key: string): T => {
+    {
+        const stored = localStorage.getItem(key);
+        if (!stored) throw new Error(`No data found for key "${key}"`);
+        return JSON.parse(stored) as T;
+    };
+};
+
+export default function App() {
+    return <>
+        <ResourceLoader resourceUrl={"/users/3"}>
+            {(resource: TUser) => <UserInfo user={resource} />}
+        </ResourceLoader>
+        <ResourceLoader resourceUrl={"/books/3"}>
+            {(resource: TBook) => <BookInfo book={resource} />}
+        </ResourceLoader>
+        <DataSource getFn={() => getDataFromServer<TUser>("/users/3")}>
+            {(resource: TUser) => <UserInfo user={resource} />}
+        </DataSource>
+        <DataSourceWithRender
+            getFn={() => getDataFromServer<TUser>("/users/3")}
+            render={(resource: TUser) => <UserInfo user={resource} />}
+        />
+        <DataSource getFn={() => getDataFromLocalStorage<TUser>("user")}>
+            {(resource: TUser) => <UserInfo user={resource} />}
+        </DataSource>
+        <DataSourceWithRender
+            getFn={() => getDataFromLocalStorage<TUser>("user")}
+            render={(resource: TUser) => <UserInfo user={resource} />}
+        />
+    </>
+}
