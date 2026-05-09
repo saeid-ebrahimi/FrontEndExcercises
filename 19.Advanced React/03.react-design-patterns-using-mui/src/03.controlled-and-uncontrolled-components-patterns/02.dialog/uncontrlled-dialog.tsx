f9768b15996867b40b6c73d94b66a357496d3899
saeid <saeidebrahimiapply@gmail.com>
2026-05-09 13:25:45 +0330

    add uncontrolled dialog

---
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/03.controlled-and-uncontrolled-components-patterns/02.dialog/uncontrlled-dialog.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, Modal } from "@mui/material";
+import { useState } from "react";
+
+export function UnControlledDialog() {
+    const [open, setOpen] = useState(true)
+    return <Dialog
+
+        fullWidth
+        sx={{ bgcolor: "lightblue" }}
+        keepMounted
+        scroll={"paper"} open={open}
+        onClose={() => { setOpen(false) }}
+
+    >
+        <DialogTitle sx={{ display: "flex", justifyContent: "space-between" }}>
+            This is Dialog title
+            <IconButton onClick={() => { setOpen(false) }}>
+                X
+            </IconButton>
+        </DialogTitle>
+        <DialogContent>
+            <DialogContentText>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur recusandae velit libero, tempora reiciendis eveniet molestias sunt, sed obcaecati nihil iusto voluptates iure atque amet adipisci veritatis! Soluta, non?
+            </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={() => { setOpen(false) }}>Close</Button>
+        </DialogActions>
+    </Dialog>
+}
--- 19.Advanced React/03.react-design-patterns-using-mui/src/practice/App.tsx
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/practice/App.tsx
@@ -1,10 +1,12 @@
 import { ControlledForm } from "../03.controlled-and-uncontrolled-components-patterns/01.forms/controlled-form"
 import { UnControlledForm } from "../03.controlled-and-uncontrolled-components-patterns/01.forms/uncontrolled-form"
+import { UnControlledDialog } from "../03.controlled-and-uncontrolled-components-patterns/02.dialog/uncontrlled-dialog"
 import "../App.css"
 
 export default function App() {
     return <>
         <UnControlledForm />
         <ControlledForm />
+        <UnControlledDialog />
     </>
 }
