95e948454ce8ff0ac04d037a75e9717cf9fe005b
saeid <saeidebrahimiapply@gmail.com>
2026-08-04 13:06:44 +0330

    add new split screen pattern

--- 19.Advanced React/03.react-design-patterns-using-mui/src/practice/App.tsx
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/practice/App.tsx
@@ -1,22 +1,11 @@
 // import { UserInfoWrapper } from "../04.HOC-design-pattern/user-info-wrapper";
 
-import { AnalyzedUserInfo } from "../04.HOC-design-pattern/components/analyzed-user-info";
+import { StackDemo } from "../01.layout-patterns/01.SplitScreen/stack";
 
 export default function App() {
 
     return <>
-        {/* <UserInfoWrapper
-            user={{ age: 23, books: [], country: "USA", name: "John" }}
-            calledIn={"List of users"}
-            utmData={{
-                utm_source: "google",
-                utm_medium: "cpc",
-                utm_campaign: "summer_sale_2026",
-                utm_term: "react hoc tutorial",
-                utm_content: "text_ad_variant_a",
-            }}
-        /> */}
-        <AnalyzedUserInfo user={{ age: 23, books: [], country: "USA", name: "John" }} />
+        <StackDemo />
     </>
 
 }
