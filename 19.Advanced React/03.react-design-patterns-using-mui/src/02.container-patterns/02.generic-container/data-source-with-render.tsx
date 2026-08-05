55fd1b98e90adfe69a498d60ad594209379a5e86
saeid <saeidebrahimiapply@gmail.com>
2026-08-06 00:29:52 +0330

    add new version to data source with render

--- 19.Advanced React/03.react-design-patterns-using-mui/src/02.container-patterns/02.generic-container/data-source-with-render.tsx
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/02.container-patterns/02.generic-container/data-source-with-render.tsx
@@ -1,3 +1,5 @@
+import { Typography } from "@mui/material";
+import axios from "axios";
 import { ReactNode, useEffect, useState } from "react";
 
 export function DataSourceWithRender<T>(
@@ -9,8 +11,6 @@ export function DataSourceWithRender<T>(
             let data;
             if (getFn) {
                 data = await getFn();
-                console.log(data);
-
             }
             setResource(data)
         })();
@@ -18,4 +18,51 @@ export function DataSourceWithRender<T>(
 
     if (!resource) return <div>Loading...</div>;
     return <>{render(resource)}</>;
+}
+
+export function DataSourceWithRender2<T>({ getFn, render }: { getFn?: () => Promise<T>; render: (prop: T) => ReactNode }) {
+    const [resource, setResource] = useState<T | null>(null);
+    const [fetchCondition, setFetchCondition] = useState<"loading" | "error" | "success">("loading");
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const getResource = async () => {
+            let data;
+            try {
+                if (getFn) {
+                    setFetchCondition("loading")
+                    data = await getFn();
+                    setResource(data);
+                    setFetchCondition("success");
+                }
+            } catch (error) {
+                if (axios.isCancel(error)) {
+                    console.log("Request canceled");
+                } else {
+                    setFetchCondition("error")
+                }
+            }
+
+        }
+        getResource()
+
+        return () => {
+            controller.abort()
+        }
+    }, [])
+
+    if (fetchCondition === "loading") {
+        return <Typography color={"textSecondary"}>Loading...</Typography>
+    } else if (fetchCondition === "error") {
+        return <Typography color={"error"}>Error in getting data</Typography>
+    } else if (fetchCondition === "success") {
+        if (!resource) {
+            return <Typography color={"textSecondary"}>Cannot find the resource...</Typography>
+        }
+        return <>
+            {render(resource)}
+        </>
+    }
+
+    return <></>
 }
