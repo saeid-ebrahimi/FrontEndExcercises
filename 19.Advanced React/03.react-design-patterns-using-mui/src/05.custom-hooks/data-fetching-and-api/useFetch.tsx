521c83cc3b262f342c12d01fd67bd8f6087260ca
saeid <saeidebrahimiapply@gmail.com>
2026-08-13 18:50:44 +0330

    change the logic of use fetch to prevent catchig cancel error

--- 19.Advanced React/03.react-design-patterns-using-mui/src/05.custom-hooks/data-fetching-and-api/useFetch.tsx
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/05.custom-hooks/data-fetching-and-api/useFetch.tsx
@@ -21,13 +21,17 @@ export function useFetch<T>(url: string): TUseFetchResult<T> {
                 setError(null);
                 const response = await axios.get<T>(url, { signal: controller.signal });
                 setDate(response.data);
-            } catch (error) {
-                let message = `error in getting data from "${url}"`
-                if (error !== axios.isCancel(error) && (error instanceof Error || axios.isAxiosError(error))) {
-                    message = error.message
+            } catch (e) {
+                if (!axios.isCancel(e)) {
+                    let message = `error in getting data from "${url}"`
+                    if ((e instanceof Error || axios.isAxiosError(e))) {
+                        message = e.message
+                    }
+                    const err = new Error(message);
+                    setError(err)
+                } else {
+                    setError(null)
                 }
-                const err = new Error(message);
-                setError(err)
             } finally {
                 setLoading(false);
             }
