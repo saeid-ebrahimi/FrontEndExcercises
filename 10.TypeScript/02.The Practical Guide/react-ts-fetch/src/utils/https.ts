abb76d8fc50348902773ade64e1e38d9ed086f7f
saeid <saeidebrahimiapply@gmail.com>
2026-08-13 18:25:55 +0330

    add deleted

--- 10.TypeScript/02.The Practical Guide/react-ts-fetch/src/utils/https.ts
+++
@@ -1,9 +0,0 @@
-export async function get(url:string) {
-    const response = await fetch(url)
-    if (!response.ok)
-    {
-        throw new Error("failed to get data!");
-    }
-    const data = await response.json() as unknown;
-    return data
-}
--- 10.TypeScript/02.The Practical Guide/react-ts-intermediate/node_modules/.vite/deps/react.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react
-} from "./chunk-3ON3CNAS.js";
-export default require_react();
-//# sourceMappingURL=react.js.map
