c059b63e4d78600633d2352b4127383a995128b0
saeid <saeidebrahimiapply@gmail.com>
2026-08-20 14:06:36 +0330

    add useTimer hook

---
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/05.custom-hooks/timers-and-interval/useTimer.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+
+type TUserTimerOptions = {
+    initialTime: number;
+    autoStart?: boolean;
+    interval?: number;
+}
+
+export function useTimer({
+    initialTime,
+    autoStart = false,
+    interval = 1000,
+}: TUserTimerOptions) {
+    const [time, setTime] = useState(initialTime);
+    const [isRunning, setIsRunning] = useState(autoStart);
+
+    const start = useCallback(() => {
+        setIsRunning(true);
+    }, []);
+
+    const pause = useCallback(() => {
+        setIsRunning(false);
+    }, []);
+
+    const reset = useCallback(() => {
+        setTime(initialTime);
+        setIsRunning(false);
+    }, [initialTime]);
+
+    useEffect(() => {
+        if (!isRunning || time < 0) return;
+        const timerId = window.setInterval(() => {
+            setTime((prev) => {
+                if (prev <= 1) {
+                    setIsRunning(false)
+                    return 0
+                }
+                return prev - 1;
+            })
+        }, interval)
+
+        return () => {
+            window.clearInterval(timerId)
+        }
+    }, [isRunning, time, interval]);
+
+    return {
+        time,
+        isRunning,
+        isFinished: time === 0,
+        start,
+        pause,
+        reset,
+    };
+}
