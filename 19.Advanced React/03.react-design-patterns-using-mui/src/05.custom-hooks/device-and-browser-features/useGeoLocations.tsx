da0465109f25f8a1d51914ee2a60edf9707816e1
saeid <saeidebrahimiapply@gmail.com>
2026-08-20 10:47:46 +0330

    add useGeoLocation

---
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/05.custom-hooks/device-and-browser-features/useGeoLocations.tsx
@@ -0,0 +1,62 @@
+"Use client";
+
+import { useCallback, useState } from "react";
+
+type TGeoLocation = {
+    latitude: number;
+    longitude: number;
+    accuracy: number;
+};
+
+type TGeoLocationState = {
+    location: TGeoLocation | null;
+    loading: boolean;
+    error: GeolocationPositionError | null;
+};
+
+export function useGeoLocation() {
+    const [geoLocationState, setGeoLocationState] = useState<TGeoLocationState>({
+        location: null,
+        loading: false,
+        error: null
+    });
+
+    const getGeoLocation = useCallback(() => {
+        if (!navigator.geolocation) {
+            return {
+                location: null,
+                loading: false,
+                error: null,
+            }
+        }
+        setGeoLocationState(prev => ({
+            ...prev,
+            loading: true,
+            error: null
+        }));
+
+        navigator.geolocation.getCurrentPosition((position) => {
+            setGeoLocationState({
+                location: {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    accuracy: position.coords.accuracy
+                },
+                loading: false,
+                error: null
+            })
+        },
+            (error) => {
+                setGeoLocationState({
+                    location: null,
+                    loading: false,
+                    error: error,
+                })
+            })
+    }, [])
+
+    return {
+        ...geoLocationState,
+        getGeoLocation
+    }
+};
