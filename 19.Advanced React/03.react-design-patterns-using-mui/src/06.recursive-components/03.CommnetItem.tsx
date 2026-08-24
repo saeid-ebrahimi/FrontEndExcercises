a7859b804553d4fe0200174fa9afa949111f6dac
saeid <saeidebrahimiapply@gmail.com>
2026-08-24 21:00:29 +0330

    change comment as recursive component

--- 19.Advanced React/03.react-design-patterns-using-mui/src/06.recursive-components/03.CommnetItem.tsx
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/06.recursive-components/03.CommnetItem.tsx
@@ -1,14 +1,18 @@
-import { Card, CardHeader } from "@mui/material";
+import { Avatar, Card, CardContent, CardHeader, Typography } from "@mui/material";
 
 export type TCommentItem = {
     id: number;
+    username: string;
     text: string;
     replies: TCommentItem[]
 }
 export function CommentItem({ comment }: { comment: TCommentItem }) {
     return (
         <Card sx={{ p: 2, m: 2 }}>
-            <CardHeader title={comment.text} />
+            <CardHeader avatar={<Avatar>{comment.username.charAt(0).toUpperCase()}</Avatar>} title={comment.username} />
+            <CardContent>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>{comment.text}</Typography>
+            </CardContent>
             <Card>
                 {comment.replies.map(reply => <CommentItem key={reply.id} comment={reply} />)}
             </Card>
--- 19.Advanced React/03.react-design-patterns-using-mui/src/practice/App.tsx
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/practice/App.tsx
@@ -52,18 +52,22 @@ const fileTree: FileNode = {
 
 const comment: TCommentItem = {
     id: 1,
+    username: "john_doe",
     text: "This is the main comment",
     replies: [
         {
             id: 2,
+            username: "jane_smith",
             text: "This is the first reply",
             replies: [
                 {
                     id: 3,
+                    username: "mike_wilson",
                     text: "This is a reply to the first reply",
                     replies: [
                         {
                             id: 4,
+                            username: "sarah_jones",
                             text: "This is a nested reply",
                             replies: [],
                         },
@@ -71,6 +75,7 @@ const comment: TCommentItem = {
                 },
                 {
                     id: 5,
+                    username: "alex_brown",
                     text: "This is another reply to the first reply",
                     replies: [],
                 },
@@ -78,6 +83,7 @@ const comment: TCommentItem = {
         },
         {
             id: 6,
+            username: "emily_davis",
             text: "This is the second reply to the main comment",
             replies: [],
         },
