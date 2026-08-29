8a5ff24efc85507ce5c7c707650016f31214fbd7
saeid <saeidebrahimiapply@gmail.com>
2026-08-29 15:21:27 +0330

    add another example of composition

---
+++ 19.Advanced React/03.react-design-patterns-using-mui/src/07.composition/07.high-order-components.tsx
@@ -0,0 +1,19 @@
+import type { ComponentType } from "react";
+
+type WithAuthProps = {
+    isAuthenticated: boolean;
+}
+
+function withAuth<P extends object>(Component: ComponentType) {
+    return function AuthenticatedComponent(props: P & WithAuthProps) {
+        const { isAuthenticated, ...componentProps } = props;
+
+        if (!isAuthenticated) {
+            return <div>Please login first.</div>;
+        }
+
+        return <Component {...(componentProps as P)} />;
+    }
+}
+
+export const AuthenticatedComponent = withAuth
