8cb1305cc61555f4a1d720bedae311d8f4958b29
saeid <saeidebrahimiapply@gmail.com>
2026-09-01 21:48:42 +0330

    working on solve redundant state in parent with heavy components

---
+++ 19.Advanced React/04.performance/src/01.re-rendering/01.redundant-state-in-parent/App.tsx
@@ -0,0 +1,14 @@
+import { AdditionalComplexThings, BlaBla } from "./components/dummy-components"
+import { SlowComponent } from "./components/slow-component"
+import ToggleButtonWithDialog from "./components/toggle-button-with-dialog";
+
+function App() {
+    return <>
+        <ToggleButtonWithDialog />
+        <SlowComponent />
+        <BlaBla />
+        <AdditionalComplexThings />
+    </>
+}
+
+export default App
--- 19.Advanced React/04.performance/src/App.tsx
+++ 19.Advanced React/04.performance/src/App.tsx
@@ -1,6 +1,13 @@
+import { AdditionalComplexThings, BlaBla } from "./01.re-rendering/01.redundant-state-in-parent/components/dummy-components"
+import { SlowComponent } from "./01.re-rendering/01.redundant-state-in-parent/components/slow-component"
+import ToggleButtonWithDialog from "./01.re-rendering/01.redundant-state-in-parent/components/toggle-button-with-dialog";
 
 function App() {
   return <>
+    <ToggleButtonWithDialog />
+    <SlowComponent />
+    <BlaBla />
+    <AdditionalComplexThings />
   </>
 }
 
