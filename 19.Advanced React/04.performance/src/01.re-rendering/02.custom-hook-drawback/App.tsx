34db8e9935ef4d4810db85017d3cf6ea439aff39
saeid <saeidebrahimiapply@gmail.com>
2026-09-01 23:39:35 +0330

    add app content

--- 19.Advanced React/04.performance/src/01.re-rendering/02.custom-hook-drawback/App.tsx
+++ 19.Advanced React/04.performance/src/01.re-rendering/02.custom-hook-drawback/App.tsx
@@ -1,7 +1,17 @@
+import { AdditionalComplexThings } from "./components/dummy-components"
+import { BlaBla } from "./components/dummy-components"
+import { SlowComponent } from "./components/slow-component"
+import ToggleButtonWithDialog from "./components/toggle-button-with-dialog";
 
 function App() {
+  // const { isVisible, show, hide } = useToggleDialog();
   return <>
-
+    {/* <Button onClick={show}>Show Dialog</Button>
+    {isVisible ? <ModalDialog onClose={hide} /> : null} */}
+    <ToggleButtonWithDialog />
+    <SlowComponent />
+    <BlaBla />
+    <AdditionalComplexThings />
   </>
 }
 
